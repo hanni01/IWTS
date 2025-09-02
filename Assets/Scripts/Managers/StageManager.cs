3847a612b96cf20d73325a6290177a4e938d8270
hanni01 <he3080@gmail.com>
2025-09-02 22:46:04 +0900

    [feat] StageManager: 스테이지 통과하였을때 클리어 처리하고, 다음 스테이지로 자동 이동되게 한다.

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class StageManager : IManager
 {
+    /// <summary>
+    /// 통과한 스테이지 씬 이름을 담아두는 변수, 솔직히 필요없긴 한데 일단 넣음
+    /// </summary>
     private static readonly HashSet<string> clearedStages = new();
 
     public List<string> _allStages;
@@ -12,6 +15,7 @@ public class StageManager : IManager
     {
         _allStages = new List<string>
         {
+            Scenes.TUTORIAL,
             Scenes.STEP1,
             Scenes.STEP2,
         };
@@ -22,23 +26,23 @@ public class StageManager : IManager
         clearedStages.Clear();
     }
 
+    /// <summary>
+    /// 스테이지 통과하였을때 호출하는 메서드
+    /// </summary>
+    /// <param name="stageName">스테이지 씬 이름</param>
     public void ClearedStage(string stageName)
     {
         clearedStages.Add(stageName);
         int currentStageIdx = _allStages.IndexOf(stageName);
         if(currentStageIdx != -1 && currentStageIdx + 1 < _allStages.Count)
         {
-
+            string nextStageName = _allStages[currentStageIdx + 1];
+            GameManager.Scene.LoadScene(nextStageName);
         }
     }
 
     public bool IsStageCleared(string stageName)
     {
         return clearedStages.Contains(stageName);
-    }
-
-    public bool AreAllStagesCleared()
-    {
-        return _allStages.All(stage => clearedStages.Contains(stage));
     }
 }
