e41cf6eb6245b332886400a9f2cda5aeca1251f3
hanni01 <he3080@gmail.com>
2025-09-02 20:43:24 +0900

    [feat] GameManager 구현 - SceneManager: Loading씬의 로딩바를 통해 비동기 씬 로딩 - StageManager: 스테이지 통과 및 모든 스테이지 완료 상태 저장(미구현)

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public static SceneManager Scene { get; } = new();
+    public static StageManager Stage { get; } = new();
+
+    private bool _isDestroyManager;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void OnApplicationQuit()
+    {
+        DestroyManager();
+    }
+
+    public static void Initialize()
+    {
+        var managers = new List<IManager>
+        {
+            Scene,
+            Stage,
+        };
+
+        foreach (var manager in managers)
+        {
+            manager.Initialize();
+        }
+    }
+
+    private void DestroyManager()
+    {
+        if (_isDestroyManager) return;
+
+        _isDestroyManager = true;
+        Release();
+    }
+
+    private void Release()
+    {
+        var managers = new List<IManager>
+        {
+            Scene,
+            Stage,
+        };
+
+        foreach(var manager in managers)
+        {
+            manager.Release();
+        }
+    }
+}
