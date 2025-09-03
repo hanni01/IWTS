f077173ebfc791eac3f01a387f459e61c52c4d3e
hanni01 <he3080@gmail.com>
2025-09-03 22:21:33 +0900

    [refact] 그냥 보기 좋게 Scenes 순서 수정

--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -4,8 +4,8 @@ public static class Scenes
 {
     public const string NONE = "None";
     public const string LOAD = "00_LoadingScene";
-    public const string TUTORIAL = "02_TutorialScene";
     public const string START = "01_StartScene";
+    public const string TUTORIAL = "02_TutorialScene";
     public const string STEP1 = "03_Step1Scene";
     public const string FINAL = "04_FinalStepScene";
     public const string TEST = "test_hallway";
