55680a66afeb6218d8c7662c9dc076c9ff7fe982
hanni01 <he3080@gmail.com>
2025-09-03 11:42:24 +0900

    [refact] 필요한 씬 네임 추가 및 Final Stage 이름 수정

--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -5,6 +5,7 @@ public static class Scenes
     public const string NONE = "None";
     public const string LOAD = "00_LoadingScene";
     public const string TUTORIAL = "02_TutorialScene";
+    public const string START = "01_StartScene";
     public const string STEP1 = "03_Step1Scene";
-    public const string STEP2 = "04_Step2Scene";
+    public const string FINAL = "04_FinalStepScene";
 }
