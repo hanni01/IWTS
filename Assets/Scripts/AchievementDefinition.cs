c48b11c03302256535cda80f899f2cd0a35d18cf
hanni01 <he3080@gmail.com>
2025-09-03 21:44:26 +0900

    [feat] AchievementDefinition ScriptableObject 생성

---
+++ Assets/Scripts/AchievementDefinition.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ACH_", menuName = "Game/Achievement Definition")]
+public class AchievementDefinition: ScriptableObject
+{
+    public int id;
+    public string achievementTitle;
+}
