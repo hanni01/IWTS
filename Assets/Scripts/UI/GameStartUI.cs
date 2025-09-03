024471df0dab535fab211edc469e30098829cfd9
hanni01 <he3080@gmail.com>
2025-09-03 10:16:41 +0900

    [feat] 아무 키 눌러서 게임 시작으로 변경, 텍스트 색상 조절 효과 추가

--- Assets/Scripts/UI/GameStartUI.cs
+++ Assets/Scripts/UI/GameStartUI.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameStartUI : MonoBehaviour
 {
     [SerializeField] private Button gameStartBtn;
-    [SerializeField] private Button gameLoadBtn;
+    [SerializeField] private Graphic target;
+    private float _speed = 1f;
 
     private void Awake()
     {
+        // 우선 클릭으로도 시작할 수 있게 둔다.
         gameStartBtn.onClick.AddListener(() =>
         {
             GameManager.Scene.LoadScene(Scenes.TUTORIAL);
         });
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            GameManager.Scene.LoadScene(Scenes.TUTORIAL);
+        }
+
+        float t = Mathf.PingPong(Time.time * _speed, 1f);
+        float a = Mathf.Lerp(30f / 200f, 1f, t);
+
+        var c = target.color;
+        c.a = a;
+        target.color = c;
     }
 }
