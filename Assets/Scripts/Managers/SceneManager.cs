b4a6d687f30e1a543f32e6edcfa2bbf50fedc2fb
hanni01 <he3080@gmail.com>
2025-09-09 00:00:23 +0900

    [refact] SceneManager 수정 - 현재 씬용 취소 토큰을 통한 비동기 진행 요소 멈추는 기능 추가

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -2,46 +2,61 @@ using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+using System.Threading;
 
 public class SceneManager : IManager
 {
     public string CurrentSceneName = Scenes.NONE;
 
     private const string LOADING_SCENE_NAME = "00_LoadingScene";
-
     private LoadingUI _loading;
+
+    // 현재 씬용 취소 토큰
+    private CancellationTokenSource _sceneCts;
 
     public void Initialize()
     {
-        
+        _sceneCts = new CancellationTokenSource();
     }
 
     public void Release()
     {
-        
+        _sceneCts?.Cancel();
+        _sceneCts?.Dispose();
     }
 
     public async void LoadScene(string sceneName)
     {
         CurrentSceneName = sceneName;
-
         await LoadSceneAsyncWithLoadScene(sceneName);
+    }
+
+    public CancellationToken GetSceneToken()
+    {
+        return _sceneCts.Token;
     }
 
     public async UniTask LoadSceneAsyncWithLoadScene(string sceneName)
     {
+        // 이전 씬 작업 중단
+        _sceneCts?.Cancel();
+        _sceneCts?.Dispose();
+
+        // 새 토큰 발급
+        _sceneCts = new CancellationTokenSource();
+
         await UnitySceneManager.LoadSceneAsync(LOADING_SCENE_NAME, LoadSceneMode.Additive);
 
         var mono = Object.FindAnyObjectByType<LoadingUI>();
 
-        if(mono is LoadingUI loadingScript)
+        if (mono is LoadingUI loadingScript)
         {
             _loading = loadingScript;
         }
 
-        if(null == _loading)
+        if (null == _loading)
         {
-            Debug.LogError("로딩 씬 내  Loading_UI를 찾지 못함");
+            Debug.LogError("로딩 씬 내 Loading_UI를 찾지 못함");
             return;
         }
 
@@ -51,7 +66,7 @@ public class SceneManager : IManager
     private async UniTask PerformSceneTransition(string sceneName)
     {
         var currentScene = UnitySceneManager.GetActiveScene().name;
-        if(string.IsNullOrEmpty(currentScene) )
+        if (string.IsNullOrEmpty(currentScene))
         {
             Debug.LogError("현재 활성화 된 씬 이름이 없습니다.");
             return;
@@ -61,19 +76,15 @@ public class SceneManager : IManager
         {
             var unloadSceneAsync = UnitySceneManager.UnloadSceneAsync(currentScene);
 
-            while(!unloadSceneAsync.isDone)
+            while (!unloadSceneAsync.isDone)
             {
                 UpdateLoadingUI($"{unloadSceneAsync.progress * 100:F0}%", unloadSceneAsync.progress);
                 await UniTask.Yield(PlayerLoopTiming.Update);
             }
         }
-        else
-        {
-            Debug.LogError($"현재 씬 {currentScene}은 이미 언로드 된 상태입니다.");
-        }
 
         var sceneLoad = UnitySceneManager.LoadSceneAsync(sceneName);
-        if(sceneLoad == null)
+        if (sceneLoad == null)
         {
             Debug.LogError($"씬 {sceneName}을 찾을 수 없습니다.");
             return;
@@ -85,7 +96,6 @@ public class SceneManager : IManager
             await UniTask.Yield(PlayerLoopTiming.Update);
         }
 
-        //씬 활성화 대기
         sceneLoad.allowSceneActivation = true;
         while (!sceneLoad.isDone)
         {
@@ -94,7 +104,6 @@ public class SceneManager : IManager
 
         SoundManager.Instance.StopLoopSFX();
 
-        //프레임 렌더링까지 기다리기
         await UniTask.WaitForEndOfFrame();
     }
 
